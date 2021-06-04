b5e43aad86e2da6728a38a5b3353364f6e6760b6
richard <wangsying@gmail.com>
2021-06-05 01:31:04 +0800

    project init

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
-const { Stage, Text, TextStyle } = ReactPixi;
+const { Stage, Text, TextStyle , TilingSprite} = ReactPixi;
 
 const App = () => (
-<Stage x={0} y={0}>
+<Stage width={375} height={812}>
+<TilingSprite
+    image={'https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/p2.jpeg'}
+    width={375}
+    height={812}
+    tilePosition={{ x: 0, y: 0 }}
+    tileScale={{ x: 0.1, y: 2 }}
+  />
 <Text
     text="Hello World"
     anchor={0.5}
@@ -10,7 +17,7 @@ const App = () => (
     style={
     new PIXI.TextStyle({
         fontSize: 42,
-        fill: "#ffffff",
+        fill: "#000000",
     })
     }
   />
